b8a86278f0fd2bf8e9abfc21fec80737e2b3529a
Max McDonough <max.mcdonough@gmail.com>
2023-06-28 10:39:40 -0400

    Added About page

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,5 +1,6 @@
-import { BrowserRouter as Router, Routes } from "react-router-dom";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Header from "./components/Header/Header";
+import About from "./pages/About/About";
 import './App.css';
 
 const App = () => {
@@ -8,6 +9,7 @@ const App = () => {
       <Header />
       <Routes>
         {/* <Route path="/" element={<Home />} /> */}
+        <Route path="/About" element={<About />} />
       </Routes>
     </Router>
   )
---
+++ client/src/pages/About/About.tsx
@@ -0,0 +1,26 @@
+// import { CardHeader, CardTitle } from "@react-md/card";
+// import { AboutContainer, AboutCard, AboutContent } from "./styles";
+
+const About = () => {
+
+  return (
+    <div>
+        <div>
+          <h2>About</h2>
+          {/* PROFESSIONAL HEADSHOT */}
+          {/* <img style={{width: "100%"}} src={"/assets/vfh-favorites-screenshot.png"} alt="favorites-screenshot" /> */}
+          <p>As a creative thinker who is always learning, I love doing and building things myself, whether it’s home renovation projects, films, or websites.</p>
+          {/* PHOTO WORKING ON FILM PROJECT */}
+          {/* <img style={{width: "100%"}} src={"/assets/vfh-home-screenshot.png"} alt="home-screenshot" /> */}
+          <p>I’ve always been passionate about storytelling, which first led me to filmmaking. After earning a Bachelor’s in Film and Digital Media from Georgia State University, I spent several years freelancing in nearly every craft in the film and television industry in Atlanta, eventually focusing on the Camera Department. Working as a Camera Operator and later a Drone pilot, I honed my photographic skills learning to keep up with ever-changing technologies.</p>
+          <p>Like many people, I spent a lot of time at home during the covid-19 pandemic, refocusing and reassessing. I started a garden, renovated my kitchen, and started teaching myself web development. After several years customizing wordpress sites, I wanted to learn the basics from the ground up. I later completed a bootcamp in Full Stack Development at Georgia Tech, specializing in Javascript ES6+, React, NodeJs, and RESTful APIs. </p>
+          {/* PHOTO - FAMILY */}
+          {/* <img style={{width: "100%"}} src={"/assets/vfh-login-screenshot.png"} alt="login-screenshot" /> */}
+          <p>Since graduating from the bootcamp I have expanded into new languages and frameworks. I learned Python and Typescript, and I have integrated mapping into numerous projects with the Javascript Maps API. In a world of ever changing technology, it’s important to realize that you will always be learning something new.</p>
+          <p>In my free time, I enjoy spending time with my daughter Sidney and partner Sarah. I also love playing music and long exposure photography.</p>
+        </div>
+    </div>
+  )
+};
+
+export default About;
