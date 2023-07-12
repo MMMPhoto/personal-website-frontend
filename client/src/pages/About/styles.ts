618f5ae1698f5d77471b3b9e1220766a17d56146
Max McDonough <max.mcdonough@gmail.com>
2023-07-12 12:40:51 -0400

    Added react-md cards, adjust About page styling and layout

--- client/src/pages/About/styles.ts
+++ client/src/pages/About/styles.ts
@@ -1,11 +1,33 @@
 import { styled } from "styled-components";
+import { Card, CardContent } from "@react-md/card";
 
 export const PageContainer = styled.div`
-  display: flex;
+  overflow: auto;
+  margin: 20px auto; 
+  /* display: flex;
   flex-direction: column;
   justify-content: center;
   align-items: center;
-  width: 100vw;
+  width: 100vw; */
+`;
+
+export const AboutCard = styled(Card)`
+  position: relative;
+  margin: 0 auto;
+  width: 90vw;
+  display: flex;
+  flex-basis: 100%;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  @media (min-width: 600px) {
+    width: 80vw;
+  }
+`;
+
+export const AboutContent = styled(CardContent)`
+  margin: 0 auto;
+  width: 50%;
 `;
 
 export const TextParagraph = styled.p`
